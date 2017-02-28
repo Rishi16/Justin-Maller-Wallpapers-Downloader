e213c842b2fc055ca40a8c9a4f1822163b0389ca
Suhas Kashyap <kashyapsuhas07@gmail.com>
2017-03-01 03:45:53 +0530

    Final version

--- facets_image_scraper.py
+++ facets_image_scraper.py
@@ -1,5 +1,5 @@
 '''
-	usage of python requests and beautifulsoup
+	simple yet functional scraper to get wallpapers from facets.la
 	<3 Justin Maller
 '''
 
@@ -10,21 +10,20 @@ from bs4 import BeautifulSoup
 
 
 def get_images():
-	# TODO:
-	# DO THE TRADITIONAL WAY'
-	# NOT ALL OF THEM ARE WALLPAPERS
-	for i in range(1, 331)[22:23]:
-		url = 'http://www.facets.la/2013/' + str(i) + '/wallpaper/'
-		print(url)
-		src = requests.get(url)
-		soup = BeautifulSoup(src.text, 'html.parser')
-		img = soup.findAll('img')[2]
-		print(img)
-
+	page_src = requests.get('http://www.facets.la/wallpapers/')
+	page_soup = BeautifulSoup(page_src.text, 'html.parser')
+	for image_link in page_soup.findAll('div', {'class', 'thumb-image'}):
+		image_url = image_link.find('a').get('href')
+		image_url_src = requests.get(image_url)
+		image_url_soup = BeautifulSoup(image_url_src.text, 'html.parser')
+		img = image_url_soup.find('div', id='facet-wallpaper').find('img').get('src')
+		title = image_url_soup.find('div', {'class', 'size15'}).find('strong').text
+		urllib.request.urlretrieve(img, folder_path + '/' + title + '.jpg')
+		print('Finished downloading ' + title)
 
 
 if __name__ == '__main__':
-	#folder_path = input('Enter a path to store these wallpapers:\n')
-	#os.makedirs(folder_path, exist_ok=True)
+	folder_path = input('Enter a path to store these wallpapers:\n')
+	os.makedirs(folder_path, exist_ok=True)
 
 	get_images()
