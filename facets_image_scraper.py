a43e3f9f2c14f6f9522595919e42f906f4fafdf6
Suhas Kashyap <kashyapsuhas07@gmail.com>
2017-03-01 06:27:31 +0530

    Used exception to handle 1 small issue
    Not a major problem. All images will still be downloaded.

--- facets_image_scraper.py
+++ facets_image_scraper.py
@@ -10,20 +10,26 @@ from bs4 import BeautifulSoup
 
 
 def get_images():
-	page_src = requests.get('http://www.facets.la/wallpapers/')
-	page_soup = BeautifulSoup(page_src.text, 'html.parser')
-	for image_link in page_soup.findAll('div', {'class', 'thumb-image'}):
-		image_url = image_link.find('a').get('href')
-		image_url_src = requests.get(image_url)
-		image_url_soup = BeautifulSoup(image_url_src.text, 'html.parser')
-		img = image_url_soup.find('div', id='facet-wallpaper').find('img').get('src')
-		title = image_url_soup.find('div', {'class', 'size15'}).find('strong').text
-		urllib.request.urlretrieve(img, folder_path + '/' + title + '.jpg')
-		print('Finished downloading ' + title)
+	try:
+		page_src = requests.get('http://www.facets.la/wallpapers/')
+		page_soup = BeautifulSoup(page_src.text, 'html.parser')
+		for image_link in page_soup.findAll('div', {'class', 'thumb-image'}):
+			try:
+				image_url = image_link.find('a').get('href')
+				image_url_src = requests.get(image_url)
+				image_url_soup = BeautifulSoup(image_url_src.text, 'html.parser')
+				img = image_url_soup.find('div', id='facet-wallpaper').find('img').get('src')
+				title = image_url_soup.find('div', {'class', 'size15'}).find('strong').text
+				urllib.request.urlretrieve(img, folder_path + '/' + title + '.jpg')
+				print('Finished downloading ' + title)
+			except:
+				pass
+	except:
+		print('some error. I\'m just going to ignore it ¯\_(ツ)_/¯')
 
 
 if __name__ == '__main__':
 	folder_path = input('Enter a path to store these wallpapers:\n')
-	os.makedirs(folder_path, exist_ok=True)
+	os.makedirs(folder_path, exist_ok=False)
 
 	get_images()
